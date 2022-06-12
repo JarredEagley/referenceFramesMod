7dffcc5d6dfae285cf17cc1289f7d977bfabda91
JarredEagley <jarred.eagley@hotmail.com>
2022-06-12 00:09:02 -0700

    Digi's networking code

---
+++ Data/Scripts/nubeees-refFrame/network/PacketBase.cs
@@ -0,0 +1,31 @@
+﻿using ProtoBuf;
+using Sandbox.ModAPI;
+
+/// <summary>
+/// Credit to Digi
+/// https://github.com/THDigi/SE-ModScript-Examples/blob/master/Data/Scripts/Examples/Example_NetworkProtobuf/PacketBase.cs
+/// </summary>
+namespace nubeees_refFrame.network
+{
+    // tag numbers in ProtoInclude collide with numbers from ProtoMember in the same class, therefore they must be unique.
+    [ProtoInclude(1000, typeof(PacketSimpleExample))]
+    [ProtoContract]
+    public abstract class PacketBase
+    {
+        // this field's value will be sent if it's not the default value.
+        // to define a default value you must use the [DefaultValue(...)] attribute.
+        [ProtoMember(1)]
+        public readonly ulong SenderId;
+
+        public PacketBase()
+        {
+            SenderId = MyAPIGateway.Multiplayer.MyId;
+        }
+
+        /// <summary>
+        /// Called when this packet is received on this machine.
+        /// </summary>
+        /// <returns>Return true if you want the packet to be sent to other clients (only works server side)</returns>
+        public abstract bool Received();
+    }
+}
---
+++ Data/Scripts/nubeees-refFrame/network/PacketSimpleExample.cs
@@ -0,0 +1,40 @@
+﻿\using ProtoBuf;
+using Sandbox.ModAPI;
+using VRage.Utils;
+
+/// <summary>
+/// Credit to Digi
+/// https://github.com/THDigi/SE-ModScript-Examples/blob/master/Data/Scripts/Examples/Example_NetworkProtobuf/PacketSimpleExample.cs
+/// </summary>
+namespace nubeees_refFrame.network
+{
+    // An example packet extending another packet.
+    // Note that it must be ProtoIncluded in PacketBase for it to work.
+    [ProtoContract]
+    public class PacketSimpleExample : PacketBase
+    {
+        // tag numbers in this class won't collide with tag numbers from the base class
+        [ProtoMember(1)]
+        public string Text;
+
+        [ProtoMember(2)]
+        public int Number;
+
+        public PacketSimpleExample() { } // Empty constructor required for deserialization
+
+        public PacketSimpleExample(string text, int number)
+        {
+            Text = text;
+            Number = number;
+        }
+
+        public override bool Received()
+        {
+            var msg = $"PacketSimpleExample received: Text='{Text}'; Number={Number}";
+            MyLog.Default.WriteLineAndConsole(msg);
+            MyAPIGateway.Utilities.ShowNotification(msg, Number);
+
+            return true; // relay packet to other clients (only works if server receives it)
+        }
+    }
+}
