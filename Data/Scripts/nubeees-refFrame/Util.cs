3c61bc5153c74eb657166844ceb2463720d0254b
JarredEagley <jarred.eagley@hotmail.com>
2022-06-12 00:08:32 -0700

    better chat debug

--- Data/Scripts/nubeees-refFrame/Util.cs
+++ Data/Scripts/nubeees-refFrame/Util.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sandbox.Game;
 using Sandbox.ModAPI;
 using VRage.Game;
 using VRageMath;
@@ -18,16 +19,17 @@ namespace nubeees_refFrame
         public static void DebugMessage(string msg)
         {
             if (!DEBUG_MODE) return;
-            MyAPIGateway.Utilities.ShowMessage("ReferenceFrames", msg);
+            //MyAPIGateway.Utilities.ShowMessage("ReferenceFrames", msg);
+            MyVisualScriptLogicProvider.SendChatMessage(msg, "ReferenceFrames");
         }
 
 
         public static void DrawDebugSphere(Vector3D position, float radius)
         {
             MatrixD matrix = MatrixD.CreateWorld(position, Vector3.Forward, Vector3.Up);
-            Color color = (Color.Lime * 0.75f).ToVector4();
+            Color color = (Color.Lime * 0.2f).ToVector4();
             float wireframethickness = 0.1f;
-            MySimpleObjectDraw.DrawTransparentSphere(ref matrix, radius, ref color, MySimpleObjectRasterizer.SolidAndWireframe, 360/15, lineThickness:wireframethickness);
+            MySimpleObjectDraw.DrawTransparentSphere(ref matrix, radius, ref color, MySimpleObjectRasterizer.Wireframe, 360/15, lineThickness:wireframethickness);
         }
     }
 }
